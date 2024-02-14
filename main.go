04515fb4a04b00036014f3b82f13bdd33e02e41c
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2024-02-14 14:56:45 +0300

    resolved bugs

--- main.go
+++ main.go
@@ -3,12 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Would you like to see specific sayings or all? Enter to see for all languages: ")
+	fmt.Println("Would you like to see specific sayings or all? Press 'Enter' to see for all languages: ")
 	fmt.Print("Languages available: ")
-	fmt.Println(GetAvailableLanguages())
+
+	availableLangs := GetAvailableLanguages()
+
+	fmt.Println(availableLangs)
 
 	var language string
 	fmt.Scanln(&language)
+
+	foundLang := false
+
+	for _, b := range availableLangs.Languages {
+		if b.Lang == language {
+			foundLang = true
+		}
+	}
+
+	if !foundLang {
+		fmt.Printf("Language %v not found, we will fetch all quotes.", language)
+		language = ""
+	}
+
+	fmt.Println("")
 
 	fmt.Println("See one quote or all quotes? Enter 1 for one quote and press 'Enter' to see all quotes.")
 	var showAllSayings string
@@ -21,4 +39,7 @@ func main() {
 	}
 
 	AfricanSaying(language, showAll)
+
+	fmt.Println("Press 'Enter' to exit: ")
+	fmt.Scanln()
 }
